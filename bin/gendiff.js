96fecf64bea99a2b239c6d1cf156eb7930a259bc
Andrey Samsonov <andrey.samsonov@gmail.com>
2021-03-01 22:29:18 +0400

    Add files arguments and --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,9 +2,17 @@
 
 const { program } = require('commander');
 
-program.description('Compares two configuration files and shows a difference.')
-program.version('1.0.0');
-program.helpOption('-h, --help', 'output usage information');
-program.parse(process.argv);
+const run = (filepath1, filepath2, options) => {
+  console.log(`The first file: ${filepath1}`);
+  console.log(`The second file: ${filepath2}`);
+  console.log(`Options: ${JSON.stringify(options)}`);
+};
 
-const options = program.opts();
+program.description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action(run);
+
+program.parse(process.argv);
