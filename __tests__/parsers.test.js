29e4cb553b5aec27d91ee7ba542aa2168e966669
Andrey Samsonov <andrey.samsonov@gmail.com>
2021-03-14 21:23:50 +0400

    Remove empty line

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,6 +1,5 @@
 import { getFormat } from '../src/parsers.js';
 
-
 test('getFormat recognizes the format of the input file', () => {
   expect(getFormat('file.yml')).toEqual('YAML');
   expect(getFormat('file.yaml')).toEqual('YAML');
